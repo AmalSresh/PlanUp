4f38959b94df34bcefc75245eafec58f292a5c37
AmalSresh <amalxsuresh@gmail.com>
2023-10-18 18:42:51 -0700

    Added basic UI elements and user text fields for login page.

---
+++ lib/components/my_textfield.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class MyTextField extends StatelessWidget{
+  final controller;
+  final String hintText;
+  final bool obscureText;
+  const MyTextField({
+    super.key,
+    required this.controller, //takes user input
+    required this.hintText,   //tells user what should go in the box
+    required this.obscureText, //if user chooses they can show what they type or hide it
+  });
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return  Padding(
+        padding:  const EdgeInsets.symmetric(horizontal: 25.0),
+        child: TextField(
+          controller: controller,
+          obscureText: obscureText,
+          decoration: InputDecoration(
+            enabledBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+            ),
+            focusedBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.black),
+            ),
+            fillColor: Colors.white,
+            filled: true,
+            hintText: hintText,
+
+          ),
+        )
+    );
+  }
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,137 @@
+import 'package:cpsc_362_project/components/my_button.dart';
+import 'package:cpsc_362_project/components/my_textfield.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+
+class LoginPage extends StatelessWidget
+{
+  LoginPage({super.key});
+
+  //text editing controllers
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  //sign user in method
+  void signUserIn() {
+
+  }
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: SafeArea(
+        child: Center (
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 50),
+        //logo
+
+                Image.asset('lib/images/logoFinal.png', height: 100,),
+        //welcome
+                const SizedBox(height: 40),
+                const Text(
+                  'Welcome back you\'ve been missed!',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                  ),
+                ),
+
+                const SizedBox(height: 25),
+        //username text
+                MyTextField(
+                  controller: usernameController,
+                  hintText: 'Username',
+                  obscureText: false,
+                ),
+
+                const SizedBox(height: 25),
+        //password
+                MyTextField(
+                  controller: passwordController,
+                  hintText: 'Password',
+                  obscureText: true,
+                ),
+
+                const SizedBox(height: 10),
+        //forgot password
+                const Padding(
+                  padding:  EdgeInsets.symmetric(horizontal: 25.0),
+                  child:  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children:[
+                    Text('Forgot Password',
+                      style: TextStyle(color: Colors.black),
+                      ),
+                    ]
+                  ),
+                ),
+
+                const SizedBox(height: 25),
+        //sign in button
+                 MyButton(
+                  onTap: signUserIn,
+                ),
+
+                //continue with google
+                const SizedBox(height: 50),
+
+                const Padding(
+                  padding:  EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child:Divider(
+                          thickness: 0.5,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Padding(
+                        padding:  EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Text(
+                          'Or continue with',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                  const SizedBox(height: 25),
+
+                  SignInButton(
+                    Buttons.Google,
+                    text: "Sign in with Google",
+                    onPressed: () {},
+                  ),
+
+                const SizedBox(height: 25),
+                //register
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Not a member?'),
+                    SizedBox(width: 4),
+                    Text('Register now',
+                    style: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.bold,
+                    ),
+                    ),
+                  ],
+                ),
+            ],
+          )
+        ),
+      ),
+    );
+  }
+
+}
