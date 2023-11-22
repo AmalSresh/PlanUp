54451c926c80b0f5589740c88374eb5f5fc38063
cliwanag07 <cliwanag@csu.fullerton.edu>
2023-11-21 18:43:42 -0800

    added functionality to survey ui, currently lacking ai and output

--- lib/components/q_and_a_box.dart
+++ lib/components/q_and_a_box.dart
@@ -32,66 +32,9 @@ class AnswerButton extends StatelessWidget {
   }
 }
 
-class QAndABox extends StatefulWidget {
-  final String question;
-  final List<String> answers;
-  final Function()? onTap;
+class QAndAInput {
+  String question;
+  List<String> answers;
 
-  const QAndABox({
-    super.key,
-    required this.question,
-    required this.answers,
-    required this.onTap,
-  });
-
-  @override
-  State<QAndABox> createState() => _QAndABoxState();
-}
-
-class _QAndABoxState extends State<QAndABox> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Quick Survey'),
-      ),
-      body: Column(
-        children: [
-          Container(
-            height: 90.0,
-            margin: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
-            alignment: Alignment.center,
-            // color: Colors.grey, // TEMPORARY DELETE LATER
-            child: Text(
-              widget.question,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          Expanded(
-            child: Container(
-              margin: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 5.0),
-              // color: Colors.blue, // TEMPORARY DELETE LATER
-              alignment: Alignment.center,
-              child: ListView.builder(
-                itemCount: widget.answers.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                    margin: const EdgeInsets.all(10.0),
-                    child: AnswerButton(
-                      onTap: widget.onTap,
-                      text: widget.answers[index],
-                    ),
-                  );
-                },
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
+  QAndAInput({required this.question, required this.answers});
 }
--- lib/pages/survey_page.dart
+++ lib/pages/survey_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:cpsc_362_project/components/q_and_a_box.dart';
 
@@ -10,16 +12,113 @@ class SurveyPage extends StatefulWidget {
 
 class _SurveyPageState extends State<SurveyPage> {
 
-  final String tQuestion = "This is a very long question this is a very long question this is a very long question?";
-  final List<String> tAnswerList = ["Answer 1","Answer 2","Answer 3", "Answer 4"];
+  QAndAInput input = QAndAInput(
+      question: "This is a very long question this is a very long question this is a very long question?",
+      answers: ["Q1 Answer 1","Q1 Answer 2","Q1 Answer 3", "Q1 Answer 4"]);
 
-  // generate question
-  void generateQ() {
+  // false by default, reset flag if generating new survey
+  bool lastQuestion = false;
 
+  // generate next question
+  void generateNextQAndA(final int index) {
+    // set lastQuestion flag to TRUE if already generated last question
+    // output question and answer to data file
+    // generate new QAndAInput and replace ... object with new question and answer
+
+    // below is just example of usage DO NOT USE IN FINAL CODE
+    print(input.answers[index]);
+
+    if (input.question == "This is long question number 2?") {
+      lastQuestion = true;
+    }
+    input.question = "This is long question number 2?";
+    input.answers = ["Q2 Answer 1","Q2 Answer 2","Q2 Answer 3", "Q2 Answer 4"];
+    // end example
+  }
+
+  // resets survey to first question
+  void resetSurvey() {
+    // REQUIRED set lastQuestion flag to false
+    lastQuestion = false;
+    // set input variable back to whatever first question and continue generating
+
+    // below is just example of usage DO NOT USE IN FINAL CODE
+    input = QAndAInput(
+        question: "This is a very long question this is a very long question this is a very long question?",
+        answers: ["Q1 Answer 1","Q1 Answer 2","Q1 Answer 3", "Q1 Answer 4"]);
+    // end example
   }
 
   @override
   Widget build(BuildContext context) {
-    return QAndABox(question: tQuestion, answers: tAnswerList, onTap: generateQ);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Quick Survey'),
+      ),
+      body:
+        (lastQuestion == false) ? qAndAColumn() : endSurvey(),
+    );
+  }
+
+  Widget qAndAColumn() {
+    return Column(
+      children: [
+        Container(
+          height: 90.0,
+          margin: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+          alignment: Alignment.center,
+          // color: Colors.grey, // TEMPORARY DELETE LATER
+          child: Text(
+            input.question,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Expanded(
+          child: Container(
+            margin: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 5.0),
+            // color: Colors.blue, // TEMPORARY DELETE LATER
+            alignment: Alignment.center,
+            child: ListView.builder(
+              itemCount: input.answers.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: const EdgeInsets.all(10.0),
+                  child: AnswerButton(
+                    onTap: () {
+                      setState(() {
+                        generateNextQAndA(index);
+                      });
+                    },
+                    text: input.answers[index],
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget endSurvey() {
+    return Center(
+        child: Container(
+          margin: const EdgeInsets.only(left: 60.0, right: 60.0),
+          height: 75.0,
+          alignment: Alignment.center,
+          child: AnswerButton(
+            onTap: () {
+              setState(() {
+                resetSurvey();
+              });
+            },
+            text: "Generate New Survey",
+          )
+      ),
+    );
   }
 }
